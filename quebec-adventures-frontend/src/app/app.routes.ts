c7d95bc10ab39a4371e0aa2ce3a0343f3f9b105b
Robin Le Saout <robin.lesaout@gmail.com>
2025-12-22 18:18:48 -0500

    feat: Création des listes d'activités avec appel au back et stockage dans la bd

--- quebec-adventures-frontend/src/app/app.routes.ts
+++ quebec-adventures-frontend/src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { ActivitiesListComponent } from './features/activities/activities-list/activities-list.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path:'**',
-        redirectTo: ''
-    }
+  { path: '', component: HomeComponent },
+  { path: 'activities', component: ActivitiesListComponent },
+  { path: '**', redirectTo: '' }
 ];
